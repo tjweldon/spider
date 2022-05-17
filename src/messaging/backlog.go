a7aeb50d9d0ac4c4144e02269ca2bccc926d370b
tweldon <thomas.weldon@legl.com>
2022-05-17 21:17:12 +0100

    documenting more of the code

--- src/messaging/backlog.go
+++ src/messaging/backlog.go
@@ -2,24 +2,31 @@ package messaging
 
 import "tjweldon/spider/src/util"
 
+// Backlog is the interface that the queue presents to a consumer.
 type Backlog[T any] interface {
 	Channel() <-chan T
 	Length() int
 }
 
+// ForkedBacklog is a Backlog implementation that duplexes the messages
+// so that they can be consumed by more than one consumer.
 type ForkedBacklog[T any] struct {
 	output   <-chan T
 	original Backlog[T]
 }
 
+// Channel is the implementation of Backlog.Channel
 func (fb *ForkedBacklog[T]) Channel() <-chan T {
 	return fb.output
 }
 
+// Length proxies back to the parent backlog
 func (fb *ForkedBacklog[T]) Length() int {
 	return fb.original.Length()
 }
 
+// Fork takes a Backlog and returns a pair of backlogs. The zero buffer size
+// means that the messages have to be consumed at the same rate
 func Fork[T any](original Backlog[T]) (Backlog[T], Backlog[T]) {
 	originalChannel := original.Channel()
 	c1, c2 := util.Split[T](originalChannel, 0)
--- src/messaging/dispatcher.go
+++ src/messaging/dispatcher.go
@@ -1,16 +1,27 @@
 package messaging
 
+// Dispatcher is the interface that the queue presents
+// to whichever process wants to send messages.
 type Dispatcher[T any] interface {
+
+	// Dispatch is the function called by the sending process to put
+	// messages onto the queue.
 	Dispatch(item T) (ok bool)
+
+	// Close indicates to downstream consumers that no more messages
+	// will be forthcoming
 	Close()
 }
 
+// DeDuplicatingDispatcher is a Dispatcher implementation that
+// will silently ignore messages with identical content
 type DeDuplicatingDispatcher[T comparable] struct {
 	dispatcher    Dispatcher[T]
 	previousItems []T
 	maxJobs       int
 }
 
+// WithDeDuplication wraps a dispatcher with a DeDuplicatingDispatcher
 func WithDeDuplication[T comparable](dispatcher Dispatcher[T]) *DeDuplicatingDispatcher[T] {
 	return &DeDuplicatingDispatcher[T]{
 		dispatcher:    dispatcher,
@@ -19,11 +30,14 @@ func WithDeDuplication[T comparable](dispatcher Dispatcher[T]) *DeDuplicatingDis
 	}
 }
 
+// SetMaxJobs is a fluent setter for the maximum number of unique URls after
+// which all messages are ignored.
 func (dd *DeDuplicatingDispatcher[T]) SetMaxJobs(max int) *DeDuplicatingDispatcher[T] {
 	dd.maxJobs = max
 	return dd
 }
 
+// Dispatch implements the deduplication and job limit.
 func (dd *DeDuplicatingDispatcher[T]) Dispatch(item T) bool {
 	// Deduplication, ignores messages that have already been sent
 	for _, prevItem := range dd.previousItems {
@@ -46,21 +60,28 @@ func (dd *DeDuplicatingDispatcher[T]) Dispatch(item T) bool {
 	return dd.dispatcher.Dispatch(item)
 }
 
+// Close is just a proxy for everything but the underlying queue
 func (dd *DeDuplicatingDispatcher[T]) Close() {
 	dd.dispatcher.Close()
 }
 
+// ReportDispatched returns a slice of all of the items sent
+// by the deduplicating dispatcher
 func (dd *DeDuplicatingDispatcher[T]) ReportDispatched() []T {
 	return dd.previousItems
 }
 
+// Validator is a function that acts as a filter for jobs
 type Validator[T any] func(job T) bool
 
+// ValidDispatcher is a configurable dispatcher that allows the
+// client code to supply the Validator filtering functions.
 type ValidDispatcher[T any] struct {
 	dispatcher Dispatcher[T]
 	validators []Validator[T]
 }
 
+// WithValidation wraps the passed dispatcher with validation filters
 func WithValidation[T any](dispatcher Dispatcher[T], validators ...Validator[T]) *ValidDispatcher[T] {
 	return &ValidDispatcher[T]{
 		dispatcher: dispatcher,
@@ -68,6 +89,9 @@ func WithValidation[T any](dispatcher Dispatcher[T], validators ...Validator[T])
 	}
 }
 
+// Dispatch just iterates over Validators and returns ok=true the first time
+// validation fails. If it doesn't fail, it calls its internal Dispatcher's
+// Dispatch method
 func (vd *ValidDispatcher[T]) Dispatch(item T) (ok bool) {
 	for _, validator := range vd.validators {
 		if !validator(item) {
@@ -82,13 +106,18 @@ func (vd *ValidDispatcher[T]) Close() {
 	vd.dispatcher.Close()
 }
 
+// PreProcessor is a method signature for a type invariant transformation
 type PreProcessor[T any] func(item T) T
 
+// PreProcessingDispatcher is a configurable Dispatcher that applies a series of
+// transformations to a job, such as prepending relative paths with a hostname.
 type PreProcessingDispatcher[T any] struct {
 	dispatcher    Dispatcher[T]
 	preProcessors []PreProcessor[T]
 }
 
+// WithPreProcessing is a function that wraps a dispatcher in a
+// PreProcessingDispatcher
 func WithPreProcessing[T any](
 	dispatcher Dispatcher[T], preProcessors ...PreProcessor[T],
 ) *PreProcessingDispatcher[T] {
